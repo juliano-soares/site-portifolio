89bfbdc77248a7010abb5ff0c3abe8f9bce2dc7c
juliano leonardo soares <julianoleonardosoares@hotmail.com>
2020-12-25 18:44:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Site para Portifolio
+
+<img src="./src/assets/images/captura.png" alt="Captura" width="1000">
---
+++ config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  siteTitle: 'Juliano Soares', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Juliano',
+  lastName: 'Soares',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/juliano-soares',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/juliano-leonardo-soares/',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/Juliano45Soares',
+    },
+    {
+      icon: 'fa-facebook-f',
+      name: 'Facebook',
+      url: 'https://www.facebook.com/julianoleonardosoares/',
+    },
+  ],
+  email: 'julianoleonardosoares@hotmail.com',
+  phone: '(55) 99956-2477',
+  address: 'Rua Dezenove, 02 - Santa Maria - RS',
+};
